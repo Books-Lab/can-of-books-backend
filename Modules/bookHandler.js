5fd223e19f390f5a45f760611a0b11747f95899a
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-06-06 21:14:11 -0700

    completed 15

--- Modules/bookHandler.js
+++ Modules/bookHandler.js
@@ -5,7 +5,7 @@ const Book = require('../Model/books');
 const bookHandler = {};
 
 bookHandler.getBooks = function (req, res) {
-  let queryObject = {};
+  let queryObject = {email: req.user.email};
 
   Book.find(queryObject)
     .then(data => res.status(200).send(data))
@@ -14,7 +14,7 @@ bookHandler.getBooks = function (req, res) {
 };
 
 bookHandler.postBooks = function (req, res, next) {
-  const data = req.body;
+  const data = {...req.body, email: req.user.email};
   Book.create(data)
     .then(createdBook => res.status(201).send(createdBook))
     .catch(err => next(err));
@@ -30,8 +30,8 @@ bookHandler.deleteBook = function(req, res, next){
 
 bookHandler.updatedBook = function(req, res, next){
   const {id} = req.params;
-  const data = req.body;
-  Book.findByIdAndUpdate(id, data, {new: true, overwrite: true})
+  // const data = req.body;
+  Book.findByIdAndUpdate(id, {...req.body, email: req.user.email}, {new: true, overwrite: true})
     .then(updatedBook => res.status(200).send(updatedBook))
     .catch(err => next(err));
 };
